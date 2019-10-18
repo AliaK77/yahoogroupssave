7acc32d6c7c1757d0d47d66b2cea8e573c082f01
AliaK77 <aliak77@gmail.com>
2019-10-19 02:47:52 +1100

    initial version

---
+++ yahoogroupssave.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+groupname=$1
+total=$2
+datestamp="$(date +"%Y-%m-%d")"
+filename="yahoogroup-"$groupname"-"$datestamp"-.html"
+for (( i=1; i <= $total; i++ ))
+do
+#  echo "date = "$datestamp
+#  echo "page number = $i"
+  filename="yahoogroup-$groupname-$datestamp-$i.html"
+  yahoogroupurl="https://groups.yahoo.com/neo/groups/"$groupname"/conversations/messages/$i"
+  echo "saving yahoo group $groupname conversation page number: $i -- url: $yahoogroupurl into $filename"
+  ###curl -sS $yahoogroupurl > $filename
+  curl ${yahoogroupurl} -sS -o $filename
+done
