2dcb8b52675682b8a907551590c82b359df0884b
AliaK77 <aliak77@gmail.com>
2019-10-21 23:44:50 +1100

    initial upload

---
+++ yhg-save-to-html.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+groupname=$1
+total=$2
+datestamp="$(date +"%Y-%m-%d")"
+filename="yahoogroup-"$groupname"-"$datestamp"-.html"
+#for (( i=1490; i <= $total; i++ ))
+for (( i=1; i <= $total; i++ ))
+do
+#  echo "date = "$datestamp
+#  echo "page number = $i"
+  filename="yahoogroup-$groupname-$datestamp-$i.html"
+  yahoogroupurl="https://groups.yahoo.com/neo/groups/"$groupname"/conversations/messages/$i"
+  echo "saving yahoo group $groupname conversation page number: $i -- url: $yahoogroupurl into $filename"
+  ###curl -sS $yahoogroupurl > $filename
+  curl ${yahoogroupurl} -sS -o $filename
+#  sleep 10
+done
